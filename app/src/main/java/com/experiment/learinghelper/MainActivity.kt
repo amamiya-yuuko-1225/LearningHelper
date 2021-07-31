99c3e3464475c37b474b41e1e81774f9b383fc60
hmc <3241126481@qq.com>
2021-07-31 09:09:48 +0800

    添加了主界面到番茄钟、每日、每周复盘的跳转

--- app/src/main/java/com/experiment/learinghelper/MainActivity.kt
+++ app/src/main/java/com/experiment/learinghelper/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.experiment.learinghelper
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,10 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.experiment.learinghelper.countdown.CountDownActivity
+import com.experiment.learinghelper.daily.DailyActivity
+import com.experiment.learinghelper.listActivity.DailyListAdapter
+import com.experiment.learinghelper.weekly.WeeklyActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -19,9 +24,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         initUI()
     }
-    private fun initUI() {
+    private fun initUI() { 
         actionBar!!.hide()
         setNav()
+        weekly_report.setOnClickListener {
+            val intent = Intent(this, WeeklyActivity::class.java)
+            startActivity(intent)
+        }
+
+        clock.setOnClickListener {
+            val intent = Intent(this, CountDownActivity::class.java)
+            startActivity(intent)
+        }
+
+        daily_report.setOnClickListener {
+            val intent = Intent(this, DailyActivity::class.java)
+            startActivity(intent)
+        }
     }
      private fun setNav() {
          val navView: BottomNavigationView = findViewById(R.id.nav_view)
